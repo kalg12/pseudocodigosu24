b24840855faf78770be5bdda1370fec6f2932737
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-02-16 09:59:22 -0600

    HTML+JS

---
+++ funciones html/funciones.js
@@ -0,0 +1,15 @@
+function areaCuadrado(lado) {
+  return lado * lado;
+}
+
+// Una funcion se invoca llamando el nombre y
+//pasando el argumento
+areaCuadrado(5);
+
+//Pero yo necesito ver el valor para ello usuare:
+//console.log
+console.log(areaCuadrado(5));
+
+// O también puedo ver una alerta en mi navegador
+//usando alert()
+alert(areaCuadrado(5));
