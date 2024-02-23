dc9a27f73eb14a9d67f596da9a4d619db03b2da6
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-02-23 10:13:54 -0600

    Add files via upload

---
+++ funciones2/expresiones.js
@@ -0,0 +1,9 @@
+function saludar() {
+  const nombre = "UNID";
+  console.log("Hola", nombre);
+}
+
+function saludarDos() {
+  const nombreDos = prompt("Ingresa un nombre");
+  console.log("Hola", nombreDos);
+}
